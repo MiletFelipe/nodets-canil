c919abdcc9c75222b181f96c6f3e74b21bc9e01b
Felipe Milet <felipe.quintavalentina.com.br>
2022-09-05 09:46:44 -0300

    Projeto finalizado v1

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,16 +1,22 @@
 import { Request, Response } from 'express';
+import { Pet } from '../models/pet';
 
 export const home = (req: Request, res: Response) => {
+    let list = Pet.getAll();
+
     res.render('pages/page', {
         homem: 'active',
         banner: {
             title: 'Todos os animais',
             background: 'allanimals.jpg'
-        }
+        },
+        list
     });
 }
 
 export const dogs = (req: Request, res: Response) => {
+    let list = Pet.getFromType('dog');
+
     let user: string = 'dogs';
     res.render('pages/page', {
         user,
@@ -18,11 +24,14 @@ export const dogs = (req: Request, res: Response) => {
         banner: {
             title: 'Todos os Cachorros',
             background: 'banner_dog.jpg'
-        }
+        },
+        list
     });
 }
 
 export const cats = (req: Request, res: Response) => {
+    let list = Pet.getFromType("cat");
+
     let user: string = 'cats';
     res.render('pages/page', {
         user,
@@ -30,11 +39,14 @@ export const cats = (req: Request, res: Response) => {
         banner: {
             title: 'Todos os Gatos',
             background: 'banner_cat.jpg'
-        }
+        },
+        list
     });
 }
 
 export const fishes = (req: Request, res: Response) => {
+    let list = Pet.getFromType('fish');
+
     let user: string = 'fishes';
     res.render('pages/page', {
         user,
@@ -42,6 +54,8 @@ export const fishes = (req: Request, res: Response) => {
         banner: {
             title: 'Todos os Peixes',
             background: 'banner_fish.jpg'
-        }
+        },
+
+        list
     });
 }
